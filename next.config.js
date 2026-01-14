2bc24c2ae3ea0ad7b8be240e734010472115b91c
Vittonic <Vittonic@proton.me>
2026-01-14 16:17:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/TRADE',
   allowedDevOrigins: ["*.preview.same-app.com"],
   images: {
     unoptimized: true,
