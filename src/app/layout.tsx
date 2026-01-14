9cefd234cf158b3995d1afb1a889cfc484d3ea61
Vittonic <Vittonic@proton.me>
2026-01-14 18:53:39 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,36 @@
 import type { Metadata } from "next";
+import { Open_Sans, Poppins } from "next/font/google";
 import "./globals.css";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-open-sans",
+});
+
+const poppins = Poppins({
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "VITTONIC - Stock Trading",
   description: "Stock trading with a TRULY comprehensive service",
 };
 
-export default function RootLayout({
+export default function RootLayout({{
   children,
-}: Readonly<{
+}: Readonly<{{
   children: React.ReactNode;
-}>) {
+}}>) {{
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${{openSans.variable}} ${{poppins.variable}}`}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={openSans.className}>{children}</body>
     </html>
   );
-}
+}}
